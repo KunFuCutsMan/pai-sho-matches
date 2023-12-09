7c889f6774cde8fdf8e3ce2f953fd014315ed058
KFCMan <leonardo.a.t.m.05@gmail.com>
2023-12-09 12:38:51 -0600

    MODIFIED: GameType.moves changed to empty array

--- src/main/kotlin/com/kfcman/models/AdevarGame.kt
+++ src/main/kotlin/com/kfcman/models/AdevarGame.kt
@@ -3,6 +3,6 @@ package com.kfcman.models
 class AdevarGame(override val notation: String, override val gameID: Int) : GameType {
 
 	override val moves: Array<String>
-		get() = TODO("Not yet implemented")
+		get() = emptyArray()
 
 }
--- src/main/kotlin/com/kfcman/models/FireGame.kt
+++ src/main/kotlin/com/kfcman/models/FireGame.kt
@@ -3,6 +3,6 @@ package com.kfcman.models
 class FireGame(override val notation: String, override val gameID: Int) : GameType {
 
 	override val moves: Array<String>
-		get() = TODO("Not yet implemented")
+		get() = emptyArray()
 
 }
